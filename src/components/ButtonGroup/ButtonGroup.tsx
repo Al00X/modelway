c6de2a43d11fa105acffec004498fcb32c859603
Ali X <al00x@outlook.com>
2023-04-17 00:20:16 +0330

    Minor updoot

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -1,4 +1,4 @@
-import Icon from '@/components/Icon/Icon';
+import { Icon } from '@/components/Icon/Icon';
 import { KeyValue } from '@/interfaces/utils.interface';
 
 export const ButtonGroup = (props: {
